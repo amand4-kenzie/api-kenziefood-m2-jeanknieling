fb6a55dbe5998bcc3a759f2057c6d16738730aa9
Raphael <macedolima22@hotmail.com>
2022-01-25 12:23:38 -0300

    filtros por categoria implementados nos botoe

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,20 @@
 import { ModelApi } from "../models/modelApi.js";
 import { ModelVitrine } from "../models/modelVitrine.js";
+import { Buscar } from "./filtroProdutos.js";
+
+const botaoTodos = document.querySelector("#everyone");
+const botaoFrutas = document.querySelector("#frutas");
+const botaoPanificadora = document.querySelector("#panificadora");
+const botaoBebidas = document.querySelector("#bebidas");
 
 const api = await ModelApi.fetchProducts()
 const ul = document.getElementById('vitrine')
 
-ModelVitrine.montarVitrine(api, ModelVitrine.template, ul)
+botaoTodos.addEventListener('click', () => filtros.buscarTodos(api));
+botaoFrutas.addEventListener('click', () => filtros.buscarFrutas(api));
+botaoPanificadora.addEventListener('click', () => filtros.buscarPanificadora(api));
+botaoBebidas.addEventListener('click', () => filtros.buscarBebidas(api));
 
+
+ModelVitrine.montarVitrine(api, ModelVitrine.template, ul)
+const filtros = new Buscar()
